37124e5642ca8a365972d845a206da3fbc8213e7
thiago araujo <devthiagoaraujo@gmail.com>
2024-03-29 15:19:03 -0300

    feat Home and Error routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./routes/Home/Home.jsx";
+import Error from "./routes/Error/Error.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import GlobalStyled from "./GlobalStyled.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyled />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
